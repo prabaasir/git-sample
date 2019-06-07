c554b26f5dea3687e0b6436ab7972b20d8547437
Carmel Infanta <carmelinfanta97@gmail.com>
2019-06-07 17:04:59 +0530

    mpc added

---
+++ src/app/mpc/mpc.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MpcComponent } from './mpc.component';
+
+describe('MpcComponent', () => {
+  let component: MpcComponent;
+  let fixture: ComponentFixture<MpcComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MpcComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MpcComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/mpc/mpc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mpc',
+  templateUrl: './mpc.component.html',
+  styleUrls: ['./mpc.component.scss']
+})
+export class MpcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
